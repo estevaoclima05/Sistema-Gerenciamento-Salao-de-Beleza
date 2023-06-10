ff20027cb97241e2bf97006c4bc50a14114fb827
Isabele Lemos <isabelelemos077@gmail.com>
2023-06-09 21:19:21 -0300

    "Pegou"

--- SalaoDeBeleza/src/Salao1.java
+++ SalaoDeBeleza/src/Salao1.java
@@ -1,7 +1,7 @@
 public class Salao1 {
     public static void main(String[] args) {
     	System.out.println("Teste 10");
-    	System.out.println("Alterado");
+    	System.out.println("Agora pegou");
     	}
     }
 
